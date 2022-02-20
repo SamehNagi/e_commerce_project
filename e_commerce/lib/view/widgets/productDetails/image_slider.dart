e1d746d21cb05c564468ad96171cbc2e19fd00a7
Sameh Nagi <eng.same7.la@gmail.com>
2022-02-20 19:31:10 +0100

    Details App Bar done

--- e_commerce/lib/view/widgets/productDetails/image_slider.dart
+++ e_commerce/lib/view/widgets/productDetails/image_slider.dart
@@ -1,4 +1,7 @@
+import 'package:badges/badges.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_commerce/logic/controllers/cart_controller.dart';
+import 'package:e_commerce/routes/routes.dart';
 import 'package:e_commerce/utils/theme.dart';
 import 'package:e_commerce/view/widgets/productDetails/color_picker.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +21,8 @@ class ImageSliders extends StatefulWidget {
 
 class _ImageSlidersState extends State<ImageSliders> {
   CarouselController carouselController = CarouselController();
+
+  final cartController = Get.find<CartController>();
 
   final List<Color> colorSelected = [
     kCOlor1,
@@ -106,6 +111,70 @@ class _ImageSlidersState extends State<ImageSliders> {
               separatorBuilder: (context, index) => const SizedBox(height: 3),
               itemCount: colorSelected.length,
             ),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.only(
+            top: 20,
+            left: 25,
+            right: 25,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              InkWell(
+                onTap: () {
+                  Get.back();
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Get.isDarkMode
+                        ? pinkClr.withOpacity(0.8)
+                        : mainColor.withOpacity(0.8),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      color: Get.isDarkMode ? Colors.black : Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Obx(
+                () => Badge(
+                  position: BadgePosition.topEnd(top: -10, end: -10),
+                  animationDuration: const Duration(milliseconds: 300),
+                  animationType: BadgeAnimationType.slide,
+                  badgeContent: Text(
+                    cartController.quantity().toString(),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  child: InkWell(
+                    onTap: () {
+                      Get.toNamed(Routes.cartScreen);
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Get.isDarkMode
+                            ? pinkClr.withOpacity(0.8)
+                            : mainColor.withOpacity(0.8),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.shopping_cart,
+                        color: Get.isDarkMode ? Colors.black : Colors.white,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ],
