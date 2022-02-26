355759f486a063a2fed87013ef3bdfe2748042b3
Sameh Nagi <eng.same7.la@gmail.com>
2022-02-26 22:44:05 +0100

    Product Details Screen -- Size Clothes Lits

--- e_commerce/lib/view/screens/product_details_screen.dart
+++ e_commerce/lib/view/screens/product_details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce/model/product_models.dart';
 import 'package:e_commerce/view/widgets/productDetails/clothes_info.dart';
 import 'package:e_commerce/view/widgets/productDetails/image_slider.dart';
+import 'package:e_commerce/view/widgets/productDetails/size_lits.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -29,7 +30,7 @@ class ProductDetailsScreen extends StatelessWidget {
                 rate: productModels.rating.rate,
                 description: productModels.description,
               ),
-              // SizeList(),
+              SizeList(),
               // AddCart(),
             ],
           ),
