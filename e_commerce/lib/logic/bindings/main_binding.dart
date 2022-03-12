b23237274b92ef23dd27bc86359b26016a3ab9c7
Sameh Nagi <eng.same7.la@gmail.com>
2022-03-12 22:25:34 +0100

    Settings Screen -- UI Design

--- e_commerce/lib/logic/bindings/main_binding.dart
+++ e_commerce/lib/logic/bindings/main_binding.dart
@@ -1,9 +1,11 @@
 import 'package:e_commerce/logic/controllers/main_controller.dart';
+import 'package:e_commerce/logic/controllers/settings_controller.dart';
 import 'package:get/get.dart';
 
 class MainBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(MainController());
+    Get.put(SettingsController());
   }
 }
--- e_commerce/lib/view/screens/settings_screen.dart
+++ e_commerce/lib/view/screens/settings_screen.dart
@@ -1,6 +1,5 @@
-import 'package:e_commerce/logic/controllers/auth_controller.dart';
-import 'package:e_commerce/logic/controllers/theme_controller.dart';
 import 'package:e_commerce/utils/theme.dart';
+import 'package:e_commerce/view/widgets/text.utils.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,68 +10,102 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.theme.backgroundColor,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {
-                ThemeController().changesTheme();
-              },
-              child: Text(
-                "Dark Mode",
-                style: TextStyle(
-                  color: Get.isDarkMode ? Colors.white : Colors.black,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            GetBuilder<AuthController>(
-              builder: (controller) {
-                return TextButton(
-                  onPressed: () {
-                    Get.defaultDialog(
-                      title: "Logout From App",
-                      titleStyle: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      middleText: "Are you sure you need to logout",
-                      middleTextStyle: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      backgroundColor: Colors.grey,
-                      radius: 10,
-                      textCancel: "NO",
-                      cancelTextColor: Colors.white,
-                      textConfirm: "YES",
-                      confirmTextColor: Colors.white,
-                      onCancel: () {
-                        Get.back();
-                      },
-                      onConfirm: () {
-                        controller.signOutFromApp();
-                      },
-                      buttonColor: Get.isDarkMode ? pinkClr : mainColor,
-                    );
-                  },
-                  child: Text(
-                    "LogOut",
-                    style: TextStyle(
-                      color: Get.isDarkMode ? Colors.white : Colors.black,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
+      body: ListView(
+        padding: const EdgeInsets.all(24),
+        children: [
+          // ProfileImage(),
+          const SizedBox(
+            height: 10,
+          ),
+          Divider(
+            color: Get.isDarkMode ? Colors.white : Colors.grey,
+            thickness: 2,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextUtils(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            text: "GENERAL",
+            color: Get.isDarkMode ? pinkClr : mainColor,
+            underLine: TextDecoration.none,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          // DarkModeWidget(),
+          const SizedBox(
+            height: 20,
+          ),
+          // LanguageWidget(),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
       ),
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       TextButton(
+      //         onPressed: () {
+      //           ThemeController().changesTheme();
+      //         },
+      //         child: Text(
+      //           "Dark Mode",
+      //           style: TextStyle(
+      //             color: Get.isDarkMode ? Colors.white : Colors.black,
+      //           ),
+      //         ),
+      //       ),
+      //       const SizedBox(
+      //         height: 20,
+      //       ),
+      //       GetBuilder<AuthController>(
+      //         builder: (controller) {
+      //           return TextButton(
+      //             onPressed: () {
+      //               Get.defaultDialog(
+      //                 title: "Logout From App",
+      //                 titleStyle: TextStyle(
+      //                   fontSize: 18,
+      //                   color: Colors.black,
+      //                   fontWeight: FontWeight.bold,
+      //                 ),
+      //                 middleText: "Are you sure you need to logout",
+      //                 middleTextStyle: TextStyle(
+      //                   fontSize: 18,
+      //                   color: Colors.black,
+      //                   fontWeight: FontWeight.bold,
+      //                 ),
+      //                 backgroundColor: Colors.grey,
+      //                 radius: 10,
+      //                 textCancel: "NO",
+      //                 cancelTextColor: Colors.white,
+      //                 textConfirm: "YES",
+      //                 confirmTextColor: Colors.white,
+      //                 onCancel: () {
+      //                   Get.back();
+      //                 },
+      //                 onConfirm: () {
+      //                   controller.signOutFromApp();
+      //                 },
+      //                 buttonColor: Get.isDarkMode ? pinkClr : mainColor,
+      //               );
+      //             },
+      //             child: Text(
+      //               "LogOut",
+      //               style: TextStyle(
+      //                 color: Get.isDarkMode ? Colors.white : Colors.black,
+      //               ),
+      //             ),
+      //           );
+      //         },
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
