2177ad010bed1b1b1b3d73fbe4f8e1ad5dfd5f7e
Sameh Nagi <eng.same7.la@gmail.com>
2022-03-12 22:59:09 +0100

    Settings Screen -- Logout Widget Button

--- e_commerce/lib/view/screens/settings_screen.dart
+++ e_commerce/lib/view/screens/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/utils/theme.dart';
 import 'package:e_commerce/view/widgets/settings/dark_mode_widget.dart';
+import 'package:e_commerce/view/widgets/settings/logout_widget.dart';
 import 'package:e_commerce/view/widgets/text.utils.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -43,71 +44,9 @@ class SettingsScreen extends StatelessWidget {
           const SizedBox(
             height: 20,
           ),
-          // LogOutWidget(),
+          LogOutWidget(),
         ],
       ),
-      // body: Center(
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: [
-      //       TextButton(
-      //         onPressed: () {
-      //           ThemeController().changesTheme();
-      //         },
-      //         child: Text(
-      //           "Dark Mode",
-      //           style: TextStyle(
-      //             color: Get.isDarkMode ? Colors.white : Colors.black,
-      //           ),
-      //         ),
-      //       ),
-      //       const SizedBox(
-      //         height: 20,
-      //       ),
-      //       GetBuilder<AuthController>(
-      //         builder: (controller) {
-      //           return TextButton(
-      //             onPressed: () {
-      //               Get.defaultDialog(
-      //                 title: "Logout From App",
-      //                 titleStyle: TextStyle(
-      //                   fontSize: 18,
-      //                   color: Colors.black,
-      //                   fontWeight: FontWeight.bold,
-      //                 ),
-      //                 middleText: "Are you sure you need to logout",
-      //                 middleTextStyle: TextStyle(
-      //                   fontSize: 18,
-      //                   color: Colors.black,
-      //                   fontWeight: FontWeight.bold,
-      //                 ),
-      //                 backgroundColor: Colors.grey,
-      //                 radius: 10,
-      //                 textCancel: "NO",
-      //                 cancelTextColor: Colors.white,
-      //                 textConfirm: "YES",
-      //                 confirmTextColor: Colors.white,
-      //                 onCancel: () {
-      //                   Get.back();
-      //                 },
-      //                 onConfirm: () {
-      //                   controller.signOutFromApp();
-      //                 },
-      //                 buttonColor: Get.isDarkMode ? pinkClr : mainColor,
-      //               );
-      //             },
-      //             child: Text(
-      //               "LogOut",
-      //               style: TextStyle(
-      //                 color: Get.isDarkMode ? Colors.white : Colors.black,
-      //               ),
-      //             ),
-      //           );
-      //         },
-      //       ),
-      //     ],
-      //   ),
-      // ),
     );
   }
 }
