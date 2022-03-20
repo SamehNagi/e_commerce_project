d1f03792f5e62bfc6a1b25fd237b11dbf49f9b74
Sameh Nagi <eng.same7.la@gmail.com>
2022-03-20 20:16:18 +0100

    Payment Screen -- Payment UI Design Part 2

--- e_commerce/lib/view/screens/payment_screen.dart
+++ e_commerce/lib/view/screens/payment_screen.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/utils/theme.dart';
 import 'package:e_commerce/view/widgets/Payment/delivery_container_widget.dart';
+import 'package:e_commerce/view/widgets/Payment/payment_method_widget.dart';
 import 'package:e_commerce/view/widgets/text.utils.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -31,11 +32,11 @@ class PaymentScreen extends StatelessWidget {
                 underLine: TextDecoration.none,
               ),
               const SizedBox(
-                height: 20,
+                height: 5,
               ),
-              DeliveryContainerWidget(),
+              const DeliveryContainerWidget(),
               const SizedBox(
-                height: 20,
+                height: 15,
               ),
               TextUtils(
                 fontSize: 24,
@@ -45,13 +46,47 @@ class PaymentScreen extends StatelessWidget {
                 underLine: TextDecoration.none,
               ),
               const SizedBox(
-                height: 20,
+                height: 5,
               ),
-              // PaymentMethodSidget(),
+              const PaymentMethodWidget(),
               const SizedBox(
-                height: 20,
+                height: 15,
               ),
-              // TotalButton(),
+              Center(
+                child: TextUtils(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  text: "Total: \$200",
+                  color: Get.isDarkMode ? Colors.white : Colors.black,
+                  underLine: TextDecoration.none,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Center(
+                child: SizedBox(
+                  height: 50,
+                  width: 150,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      elevation: 0,
+                      primary: Get.isDarkMode ? pinkClr : mainColor,
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Pay Now",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
